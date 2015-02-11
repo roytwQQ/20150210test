35c8384025b7361462811ca138d070f7b58b94af
roytwQQ <roysashimi@gmail.com>
2015-02-11 14:49:02 +0800

    ertert

--- app/src/main/java/com/example/roy/nfc_test/MainActivity.java
+++ app/src/main/java/com/example/roy/nfc_test/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.roy.nfc_test;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+
     }
 
 
@@ -35,5 +47,62 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        // 處理由Android系統送出應用程式處理的intent filter內容
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            // 取得NdefMessage
+            NdefMessage[] messages = getNdefMessages(getIntent());
+            // 取得實際的內容
+            byte[] payload = messages[0].getRecords()[0].getPayload();
+            setNoteBody(new String(payload));
+            // 往下送出該intent給其他的處理對象
+            setIntent(new Intent());
+        }
+
+    }
+
+
+    NdefMessage[] getNdefMessages(Intent intent) {
+        // Parse the intent
+        NdefMessage[] msgs = null;
+        String action = intent.getAction();
+        // 識別目前的action為何
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            // 取得parcelabelarrry的資料
+            Parcelable[] rawMsgs =
+                    intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            // 取出的內容如果不為null，將parcelable轉成ndefmessage
+            if (rawMsgs != null) {
+                msgs = new NdefMessage[rawMsgs.length];
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    msgs[i] = (NdefMessage) rawMsgs[i];
+                }
+            } else {
+                // Unknown tag type
+                byte[] empty = new byte[] {};
+                NdefRecord record = new NdefRecord(NdefRecord.TNF_UNKNOWN, empty, empty, empty);
+                NdefMessage msg = new NdefMessage(new NdefRecord[] {
+                        record
+                });
+                msgs = new NdefMessage[] {
+                        msg
+                };
+            }
+        } else {
+            Log.d("Roy", "Unknown intent.");
+            finish();
+        }
+        return msgs;
+    }
+
+    private void setNoteBody(String body) {
+        textView.setText(body);
     }
 }
